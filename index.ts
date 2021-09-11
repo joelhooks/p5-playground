17652a06fac9173d238425d4a3a32437ec66a3f8
Joel Hooks <joelhooks@gmail.com>
2021-09-11 10:50:05 -0700

    do some config for typescript

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-import p5 from 'p5';
+import p5 from 'p5'
 
-const sketch = (s) => {
-    s.setup = () => {
-        s.createCanvas(100, 100, s.WEBGL);
-    }
+new p5((s: p5) => {
+  s.setup = () => {
+    s.createCanvas(500, 500, s.WEBGL)
+  }
 
-    s.draw = () => {
-        s.background(0);
-        s.circle(10, 10, 10);
-    }
-}
-
-const sketchInstance = new p5(sketch);
+  s.draw = () => {
+    s.background(0)
+    s.push()
+    s.rotate(90)
+    s.translate(5, 5, 5)
+    s.circle(10, 10, 100)
+    s.pop()
+  }
+})
