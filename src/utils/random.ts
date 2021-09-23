f57e5fdbffe7a5d7af1886b2326d967a78777970
Joel Hooks <joelhooks@gmail.com>
2021-09-23 14:01:33 -0700

    credit where it is due

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,3 +1,9 @@
+/**
+ * Generates a 64 character random seed to use for grabbing
+ * random value ranges.
+ *
+ * @see https://vimeo.com/611927853/3982dfe200 where Josh Davis explains it.
+ */
 export class Random {
   static generateRandomSeed(size: number) {
     return [...Array(size)]
